48bf8510d9dbfbb684e1f852cd7fb4678f311d4b
NibbleByte <NibbleByte3@gmail.com>
2020-10-26 16:39:08 +0200

    UI Expose classes - Debug & Magic methods. For debugging.

---
+++ Assets/DevLocker/Utils/ExposeUnityDebug.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace DevLocker.Utils
+{
+	/// <summary>
+	/// Exposes Unity Debug methods so they can be called by UnityEvent.
+	/// </summary>
+	public class ExposeUnityDebug : MonoBehaviour
+	{
+		public void Log(string message)
+		{
+			Debug.Log(message, this);
+		}
+
+		public void LogWarning(string message)
+		{
+			Debug.LogWarning(message, this);
+		}
+
+		public void LogError(string message)
+		{
+			Debug.LogError(message, this);
+		}
+
+		public void Break()
+		{
+			Debug.Break();
+		}
+
+		public void ClearDeveloperConsole()
+		{
+			Debug.ClearDeveloperConsole();
+		}
+	}
+}
---
+++ Assets/DevLocker/Utils/UnityMagicMethodsLogger.cs
@@ -0,0 +1,37 @@
+﻿using DevLocker.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityMagicMethodsLogger : MonoBehaviour, IActivatedByForce
+{
+	private void Awake()
+	{
+		LogMagic("Awake ");
+	}
+
+	void Start()
+	{
+		LogMagic("Start");
+	}
+
+	private void OnEnable()
+	{
+		LogMagic("OnEnable");
+	}
+
+	private void OnDisable()
+	{
+		LogMagic("OnDisable");
+	}
+
+	private void OnDestroy()
+	{
+		LogMagic("OnDestroy");
+	}
+
+	private void LogMagic(string message)
+	{
+		Debug.Log($"{Time.frameCount} {name} - {message}", this);
+	}
+}
