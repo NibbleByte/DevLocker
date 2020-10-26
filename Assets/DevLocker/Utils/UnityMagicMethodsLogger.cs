d63fe7b0db1c0b3614825c7755888758ad302d2b
NibbleByte <NibbleByte3@gmail.com>
2020-10-26 16:43:24 +0200

    Fixed compiler error due to left overs.

--- Assets/DevLocker/Utils/UnityMagicMethodsLogger.cs
+++ Assets/DevLocker/Utils/UnityMagicMethodsLogger.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnityMagicMethodsLogger : MonoBehaviour, IActivatedByForce
+public class UnityMagicMethodsLogger : MonoBehaviour
 {
 	private void Awake()
 	{
